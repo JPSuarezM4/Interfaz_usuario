96f863f03a3372d014af0a8f2f8f16aa8e548bcb
Juan Suarez <jpsuarez1220@gmail.com>
2023-09-17 21:54:40 -0500

    primer commit

---
+++ app/src/main/java/com/jpsm/pro_layaout/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.jpsm.pro_layaout;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword, etEmail;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+}
